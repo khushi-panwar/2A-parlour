21e63a33b646d933a1c4adaebf56a01c21a053a3
khushi panwar <125784585+khushi-panwar@users.noreply.github.com>
2024-01-22 22:57:18 +0530

    made functionality of theme changing on button click

--- script.js
+++ script.js
@@ -1,22 +1,43 @@
+
+
+// changing the themes of web page
 document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('beauty-parlor-form');
-    const inputFields = Array.from(document.querySelectorAll('#beauty-parlor-form input'));
-  
-    // Add your complex validation logic here
-  
-    form.addEventListener('submit', function (event) {
-      if (!form.checkValidity()) {
-        event.preventDefault();
-        event.stopPropagation();
-      }
-  
-      form.classList.add('was-validated');
-    });
-  
-    inputFields.forEach(function (field) {
-      field.addEventListener('input', function () {
-        field.setCustomValidity('');
-        field.classList.remove('is-invalid');
-      });
+  // Get theme buttons
+  const themeButtons = document.querySelectorAll('.theme-btn');
+  // Check local storage for the selected theme
+  const savedTheme = localStorage.getItem('selectedTheme');
+
+  // If a theme is saved, apply it
+  if (savedTheme) {
+    document.body.className = savedTheme;
+  }
+
+  themeButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      // Get button text content
+      const theme = this.textContent.toLowerCase();
+      document.body.className = `theme-${theme}`;
+      // Save the selected theme to local storage
+      localStorage.setItem('selectedTheme', `theme-${theme}`);
+
+      // Change the logo based on the selected theme
+      updateLogo(theme);
     });
   });
+});
+
+
+function updateLogo(theme) {
+  const logoElement = document.querySelector('.logo');
+  console.log(logoElement);
+
+  
+  if (theme === 'blue' ) {
+    logoElement.src = './Screenshot_2024-01-21_162256-removebg.png'; 
+  } else if (theme === 'pink' ) {
+    logoElement.src = './Screenshot_2024-01-21_162256-removebg.png'; 
+  } else {
+    // default logo
+    logoElement.src = './Screenshot_2024-01-21_162816-removebg-preview (1).png'; 
+  }
+};
